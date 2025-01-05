8dc0d90d63ca9ebaf1fe70b31f50af0068f1ca56
Tiger Abrodi <tigerabrodi@gmail.com>
2025-01-05 13:50:01 +0100

    upgrade to vite 6 and try foxiing it

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,28 @@
 import { reactRouter } from "@react-router/dev/vite";
 import autoprefixer from "autoprefixer";
 import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
+import type { Plugin } from "vite";
+import { defaultClientConditions, defaultServerConditions, defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+// Make it type-safe by explicitly typing it as a Vite Plugin
+const prismaFixPlugin: Plugin = {
+  name: 'prisma-fix',
+  enforce: 'post', // Now TypeScript knows this must be 'pre' | 'post' | undefined
+  config() {
+    return {
+      resolve: {
+        conditions: [...defaultClientConditions],
+      },
+      ssr: {
+        resolve: {
+          conditions: [...defaultServerConditions],
+          externalConditions: [...defaultServerConditions]
+        }
+      }
+    }
+  }
+};
 
 export default defineConfig(({ isSsrBuild, command }) => ({
   build: {
@@ -20,5 +40,5 @@ export default defineConfig(({ isSsrBuild, command }) => ({
   ssr: {
     noExternal: command === "build" ? true : undefined,
   },
-  plugins: [reactRouter(), tsconfigPaths()],
+  plugins: [prismaFixPlugin, reactRouter(), tsconfigPaths()],
 }));
